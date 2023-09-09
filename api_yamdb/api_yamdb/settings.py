aa7f5531f5fe5aacab02df965b0fe5a8b5fb4e29
sofiya <118591968+sofiya05@users.noreply.github.com>
2023-09-09 03:45:16 +0300

    поправила загрузку csv в бд + файлы с данными, теперь информацию в бд нужно заливать ими. Поправила модель и вьюшку для комментов, они были завязаны на тайтлах а комменты у нас надо писать на ревью. Поправила ошибку с возможностью создать несколько ревью одним автором на одно и то же произведение. Также исправила ошибку с именем приложения reviews, обновила settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'titles.apps.TitlesConfig',
+    'reviews.apps.ReviewsConfig',
     'core.apps.CoreConfig',
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from users.models import User
 
 
-class Genres(models.Model):
+class Genre(models.Model):
     '''Модель жанров'''
 
     name = models.CharField('Имя жанра', max_length=20)
@@ -15,7 +15,7 @@ class Genres(models.Model):
         return self.name
 
 
-class Categories(models.Model):
+class Category(models.Model):
     '''Модель категорий'''
 
     name = models.CharField('Имя категории', max_length=256)
@@ -25,22 +25,22 @@ class Categories(models.Model):
         return self.name
 
 
-class Titles(models.Model):
+class Title(models.Model):
     '''Модель тайтлов'''
 
     name = models.CharField('Название произведения', max_length=256)
     year = models.IntegerField('Год выпуска')
     description = models.TextField('Описание', blank=True, null=True)
     genre = models.ManyToManyField(
-        Genres, verbose_name='Жанры', through='TitlesGenre'
+        Genre, verbose_name='Жанры', through='TitlesGenre'
     )
     category = models.ForeignKey(
-        Categories,
+        Category,
         on_delete=models.CASCADE,
         verbose_name='Категория',
         related_name='titles',
     )
-    rating = models.IntegerField('Рейтинг', null=True)
+    rating = models.IntegerField(null=True)
 
     def __str__(self) -> str:
         return self.name
@@ -48,40 +48,48 @@ class Titles(models.Model):
 
 class TitlesGenre(models.Model):
     genre = models.ForeignKey(
-        Genres, on_delete=models.CASCADE, related_name='genres'
+        Genre, on_delete=models.CASCADE, related_name='genres'
     )
     title = models.ForeignKey(
-        Titles, on_delete=models.CASCADE, related_name='titles'
+        Title, on_delete=models.CASCADE, related_name='titles'
     )
 
     def __str__(self):
         return f'{self.genre} {self.title}'
 
 
-class Comments(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments'
-    )
-    title = models.ForeignKey(
-        Titles, on_delete=models.CASCADE, related_name='comments'
-    )
-    text = models.TextField(blank=False)
-    pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True
-    )
-
-
-class Reviews(models.Model):
+class Review(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews'
     )
     title = models.ForeignKey(
-        Titles, on_delete=models.CASCADE, related_name='reviews'
+        Title, on_delete=models.CASCADE, related_name='reviews'
     )
     text = models.TextField()
     score = models.IntegerField(
         validators=[MaxValueValidator(10), MinValueValidator(1)], blank=False
     )
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author_title',
+            )
+        ]
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField(blank=False)
     pub_date = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
